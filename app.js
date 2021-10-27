7152e3591d268026ebf1ade3945b5dd65f83f735
hiagrawal <agrawal.hina13@gmail.com>
2021-10-27 18:19:09 +0530

    CORS Errors and Allowing CORS

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 //app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json
 
+//this is required so that request from other domain can access our server, methods and they dont get CORS error
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //allow access from all domains //can mention specific domain also
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); //can specify all or whatever methods we want request from other domain to access
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //this is headers client side code (frontend) might set on their requests
+    next();
+})
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
